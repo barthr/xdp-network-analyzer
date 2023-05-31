a372f1d3ec1d7e7cb7847833e7c9c21a11ce38c7
barthr <info@bartfokker.com>
2023-05-31 22:59:47 +0200

    Add cursor init for skb buffer (so it can be used as socket filter)

--- src/cursor.c
+++ src/cursor.c
@@ -8,10 +8,18 @@ typedef struct {
     void* end;
 } cursor;
 
-static __always_inline cursor cursor_init(struct xdp_md* ctx)
+static cursor cursor_init(struct xdp_md* ctx)
 {
     return (cursor) {
         .end = (void*)(long)ctx->data_end,
         .pos = (void*)(long)ctx->data
+    };
+}
+
+static cursor cursor_init_skb(struct __sk_buff* buff)
+{
+    return (cursor) {
+        .end = (void*)(long)buff->data_end,
+        .pos = (void*)(long)buff->data
     };
 }
