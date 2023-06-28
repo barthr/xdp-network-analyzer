25c1ab3312d4f84f8a8d8f1c6cef40210708f187
barthr <info@bartfokker.com>
2023-06-28 23:23:37 +0200

    attach u probe to function when monitoring pid

--- ui/pid_monitor_view.go
+++ ui/pid_monitor_view.go
@@ -3,8 +3,8 @@ package ui
 import (
 	"fmt"
 	"log"
+	"xdp-network-analyzer/bpfutil"
 	"xdp-network-analyzer/process"
-	"xdp-network-analyzer/repository"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -24,9 +24,28 @@ func (p *PidMonitorModel) Init() tea.Cmd {
 	if err != nil {
 		log.Fatalf("Expected process with pid %d to be present: %s", p.pid, err)
 	}
-	if err := repository.Pid.Save(p.pid); err != nil {
-		log.Fatalf("Failed saving pid %d to monitor dns: %s", p.pid, err)
+	// if err := repository.Pid.Save(p.pid); err != nil {
+	// 	log.Fatalf("Failed saving pid %d to monitor dns: %s", p.pid, err)
+	// }
+	probe := bpfutil.Uprobe{
+		Executable: "/lib64/libc.so.6",
+		Symbol:     "getaddrinfo",
 	}
+	if err := probe.LoadProgram(bpfutil.DnsModule, "inspect_dns_lookup"); err != nil {
+		log.Fatalf("Failed loading program from module for pid %d to monitor dns: %s", p.pid, err)
+	}
+	if err := probe.Attach(bpfutil.PROBE_TYPE_ENTRY, p.pid); err != nil {
+		log.Fatalf("Failed attaching probe for pid %d to monitor dns: %s", p.pid, err)
+	}
+
+	// returnProbe := probe.Clone()
+	// if err := returnProbe.LoadProgram(bpfutil.DnsModule, "inspect_dns_response"); err != nil {
+	// 	log.Fatalf("Failed loading program from module for pid %d to monitor dns: %s", p.pid, err)
+	// }
+	// if err := returnProbe.Attach(bpfutil.PROBE_TYPE_RETURN, p.pid); err != nil {
+	// 	log.Fatalf("Failed attaching probe for pid %d to monitor dns: %s", p.pid, err)
+	// }
+
 	return nil
 }
 
